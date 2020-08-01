bb905b92a3fe1f174aa260a1d9a4f2a4223272dd
jacky_b <38528533+bancangyanyu@users.noreply.github.com>
2020-08-01 19:06:11 +0800

    11
    1

--- demo/apis/common.js
+++ demo/apis/common.js
@@ -2,7 +2,7 @@ export const PRO = {
 	dev: 'http://127.0.0.1:8081/', 
 	pro: 'https://xxx/' 
 }
-export const URL = 'http://vue.codeartsian.cn/'
+export const URL = 'xx'
 export const APPID = 'wx765ff68aaefbf59d'
 // export const local = window.location.href
   
